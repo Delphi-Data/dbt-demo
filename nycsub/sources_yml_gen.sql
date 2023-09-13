138d84c6d58bb7643c85c6fd7825d3fe51e928b1
djayatillake <dave665@gmail.com>
2023-09-13 15:25:57 +0100

    removing nyc refs

--- nycsub/sources_yml_gen.sql
+++
@@ -1,7 +0,0 @@
-{{ codegen.generate_source(
-    schema_name='austin_bikeshare',
-    generate_columns=true,
-    include_descriptions=true,
-    include_data_types=true,
-    name='austin_bike',
-    include_schema=true) }}
